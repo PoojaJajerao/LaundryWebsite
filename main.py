39ee64a68c91e98ffb6601182fe9c8874b37c7b6
premsai <143premsai@gmail.com>
2023-06-01 23:32:26 -0400

    python flask project setup

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+import connexion
+from flask import render_template
+
+logging.basicConfig(level=logging.INFO)
+app = connexion.FlaskApp(__name__, specification_dir='specs/')
+
+app.template_folder = 'templates'
+app.app.logger.info("Flask logger ready!")
+
+app.add_api(
+    'campus-laundry.yaml',
+    arguments={'title': 'campus-laundry'}
+)
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+application = app.app
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8089)
